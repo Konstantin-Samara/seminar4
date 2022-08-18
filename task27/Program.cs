824d9d0e9d7a963c159574f67d3f427cc273811e
Konstantin <kons1979@mail.ru>
2022-08-18 13:03:54 +0400

    task27 finished

---
+++ task27/Program.cs
@@ -0,0 +1,18 @@
+﻿// Задача 27: Напишите программу, которая принимает на вход число и 
+// выдаёт сумму цифр в числе.
+
+// 452 -> 11
+
+// 82 -> 10
+
+// 9012 -> 12
+
+Console.Write("Введите число : ");
+int Number = Convert.ToInt32(Console.ReadLine());
+if (Number>=0) {
+    int sum=0;
+    while (Number>=1) {
+        sum=sum+Number%10;
+        Number=Number/10; }
+    Console.WriteLine($"Результат : {sum}");}
+else Console.WriteLine($"Введенное число : {Number} не натуральное");
